dfe23332be18ed830a956cfcccee1d85c8876079
DirenWitharana <108315906+DirenWitharana@users.noreply.github.com>
2023-03-30 00:41:19 +0530

    Delete DSA_1/DSA_FX directory

--- DSA_1/DSA_FX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.dsa_fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.dsa_fx to javafx.fxml;
-    exports com.example.dsa_fx;
-}
